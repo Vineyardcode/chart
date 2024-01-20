1ab5be5a35750c68975976e16e8ee2c7d4f5d45a
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2024-01-20 12:22:49 +0100

    master

--- chart.js
+++ chart.js
@@ -4,6 +4,7 @@ class Chart{
 
     this.axesLabels=options.axesLabels;
     this.styles=options.styles;
+    this.icon=options.icon
 
     this.canvas=document.createElement('canvas');
     this.canvas.width=options.size;
@@ -14,12 +15,134 @@ class Chart{
     this.ctx=this.canvas.getContext('2d');
 
     this.margin=options.size*0.1;
-    this.transparency=0.5;
+    this.transparency=0.7;
+
+    this.dataTrans={
+      offset:[0,0],
+      scale:1
+    };
+    this.dragInfo={
+      start:[0,0],
+      end:[0,0],
+      offset:[0,0],
+      dragging:false
+    };
 
     this.pixelBounds=this.#getPixelBounds();
     this.dataBounds=this.#getDataBounds();
+    this.defaultDataBounds=this.#getDataBounds();
 
     this.#draw();
+    this.#addEventListeners();
+  }
+
+  #addEventListeners(){
+    const {canvas,dataTrans,dragInfo}=this;
+    canvas.onmousedown=(evt)=>{
+      const dataLoc=this.#getMouse(evt,true);
+      dragInfo.start=dataLoc;
+      dragInfo.dragging=true;
+    }
+    canvas.onmousemove=(evt)=>{
+      if(dragInfo.dragging){
+        const dataLoc=this.#getMouse(evt,true);
+        dragInfo.end=dataLoc;
+        dragInfo.offset=math.scale(
+          math.subtract(
+            dragInfo.start,dragInfo.end
+          ),
+        dataTrans.scale
+        );
+
+        const newOffset=math.add(
+          dataTrans.offset,
+          dragInfo.offset
+        );
+        this.#updateDataBounds(
+          newOffset,
+          dataTrans.scale
+
+        );
+        this.#draw();
+      }
+    }
+    canvas.onmouseup=()=>{
+      dataTrans.offset=math.add(
+        dataTrans.offset,
+        dragInfo.offset
+      );
+      dragInfo.dragging=false;
+    }
+    canvas.onwheel=(evt)=>{
+      const dir=Math.sign(evt.deltaY);
+      const step=0.02;
+      dataTrans.scale+=dir*step;
+      dataTrans.scale=Math.max(step,
+        Math.min(2,dataTrans.scale)
+      );
+
+      this.#updateDataBounds(
+        dataTrans.offset,
+        dataTrans.scale
+      );
+
+      this.#draw();
+      evt.preventDefault();
+    }
+  }
+
+  #updateDataBounds(offset,scale){
+    const {dataBounds,defaultDataBounds:def}=this;
+    dataBounds.left=def.left+offset[0];
+    dataBounds.right=def.right+offset[0];
+    dataBounds.top=def.top+offset[1];
+    dataBounds.bottom=def.bottom+offset[1];
+
+    const center=[
+      (dataBounds.left+dataBounds.right)/2,
+      (dataBounds.top+dataBounds.bottom)/2
+    ];
+
+    dataBounds.left=math.lerp(
+      center[0],
+      dataBounds.left,
+      scale**2
+    );
+
+    dataBounds.right=math.lerp(
+      center[0],
+      dataBounds.right,
+      scale**2
+    );
+
+    dataBounds.top=math.lerp(
+      center[1],
+      dataBounds.top,
+      scale**2
+    );
+
+    dataBounds.bottom=math.lerp(
+      center[1],
+      dataBounds.bottom,
+      scale**2
+    );
+  }
+
+  #getMouse=(evt,dataSpace=false)=>{
+    const rect=this.canvas.getBoundingClientRect();
+    const pixelLoc=[
+      evt.clientX-rect.left,
+      evt.clientY-rect.top
+    ];
+    if(dataSpace){
+      const dataLoc=math.remapPoint(
+        this.pixelBounds,
+        this.defaultDataBounds,
+        pixelLoc
+      );
+      return dataLoc
+    }
+    return pixelLoc
   }
 
   #getPixelBounds(){
@@ -49,4 +172,119 @@ class Chart{
     };
     return bounds;
   }
+
+  #draw(){
+    const {ctx,canvas}=this;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    this.#drawAxes();
+    ctx.globalAlpha=this.transparency;
+    this.#drawSamples();
+    ctx.globalAlpha=1;
+
+  }
+
+  #drawAxes(){
+    const {ctx,canvas,axesLabels,margin}=this;
+    const {left,right,top,bottom}=this.pixelBounds;
+
+    graphics.drawText(ctx,{
+      text:axesLabels[0],
+      loc:[canvas.width/2,bottom+margin/2],
+      size:margin*0.6
+    });
+
+    ctx.save();
+    ctx.translate(left-margin/2,canvas.height/2);
+    ctx.rotate(-Math.PI/2);
+    graphics.drawText(ctx,{
+      text:axesLabels[1],
+      loc:[0,0],
+      size:margin*0.6
+    });
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.moveTo(left,top);
+    ctx.lineTo(left,bottom);
+    ctx.lineTo(right,bottom);
+    ctx.setLineDash([5,4]);
+    ctx.lineWidth=2;
+    ctx.strokeStyle='lightgray';
+    ctx.stroke();
+    ctx.setLineDash([]);
+
+    const dataMin=math.remapPoint(
+      this.pixelBounds,
+      this.dataBounds,
+      [left,bottom]
+    );
+    graphics.drawText(ctx,{
+      text:math.formatNumber(dataMin[0],2),
+      loc:[left,bottom],
+      size:margin*0.3,
+      aling:'left',
+      vAlign:'top'
+    });
+    ctx.save();
+    ctx.translate(left,bottom);
+    ctx.rotate(-Math.PI/2);
+    graphics.drawText(ctx,{
+      text:math.formatNumber(dataMin[1],2),
+      loc:[0,0],
+      size:margin*0.3,
+      aling:'left',
+      vAlign:'bottom'
+    });
+    ctx.restore();
+
+    const dataMax=math.remapPoint(
+      this.pixelBounds,
+      this.dataBounds,
+      [right,top]
+    );
+    graphics.drawText(ctx,{
+      text:math.formatNumber(dataMax[0],2),
+      loc:[right,bottom],
+      size:margin*0.3,
+      aling:'right',
+      vAlign:'top'
+    });
+    ctx.save();
+    ctx.translate(left,top);
+    ctx.rotate(-Math.PI/2);
+    graphics.drawText(ctx,{
+      text:math.formatNumber(dataMax[1],2),
+      loc:[0,0],
+      size:margin*0.3,
+      aling:'right',
+      vAlign:'bottom'
+    });
+    ctx.restore();
+  }
+
+  #drawSamples(){
+    const {ctx,samples,dataBounds,pixelBounds}=this;
+    for(const sample of samples){
+      const {point,label}=sample;
+      const pixelLoc=math.remapPoint(
+        dataBounds,pixelBounds,point
+      );
+      switch(this.icon){
+        case 'text':
+          graphics.drawText(ctx,{
+            text:this.styles[label].text,
+            loc:pixelLoc,
+            size:20
+          });
+          break;
+        default:
+          graphics.drawPoint(ctx,pixelLoc,
+            this.styles[label].color);
+          break;
+      }
+    }
+  }
+
+
 }
---
+++ graphics.js
@@ -0,0 +1,47 @@
+const graphics={};
+
+graphics.drawPoint=(ctx,loc,color="black",size=8)=>{
+  ctx.beginPath();
+  ctx.fillStyle=color;
+  ctx.arc(...loc,size/2,0,Math.PI*2);
+  ctx.fill();
+}
+
+graphics.drawText=(ctx,{text,loc,align='center,',vAlign='middle',size=10,color='black'})=>{
+  ctx.textAlign=align;
+  ctx.textBaseline=vAlign;
+  ctx.font='bold '+size+'px Courier';
+  ctx.fillStyle=color;
+  ctx.fillText(text,...loc);
+}
+
+graphics.generateImages=(styles,size=20)=>{
+  for(let style of styles){
+    const canvas=document.createElement('canvas');
+    canvas.width=size;
+    canvas.height=size;
+
+    const ctx=canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.textAlign='center';
+    ctx.textBaseline='middle';
+    ctx.font=size+'px Courier';
+
+    ctx.fillText(style.text,
+      canvas.width/2,canvas.height/2)
+
+    style['image']=new Image();
+    style['image'].src=canvas.toDataURL();
+  }
+}
+
+graphics.drawImage=(ctx,image,loc)=>{
+  ctx.beginPath();
+  ctx.drawImage(image,
+    loc[0]-image.width/2,
+    loc[1]-image.height/2,
+    image.width,
+    image.height
+  );
+  ctx.fill();
+}
